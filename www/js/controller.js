7ca9c86b4181a4e6492289b22661ec79c3486115
blackjmxx <alandou.jeanmichel@gmail.com>
2015-03-15 15:44:36 +0100

    first commit

---
+++ www/js/controller.js
@@ -0,0 +1,10 @@
+var db = null;
+
+
+hankerApp.controller("WelcomeController",function($scope, $ionicPlatform, $ionicLoading, $location, $ionicHistory, $cordovaSQLite) {
+ 
+});
+
+hankerApp.controller("ProjectController",function($scope, $ionicPlatform, $ionicLoading, $location, $ionicHistory, $cordovaSQLite) {
+ 
+});
